01cfb17d0e60d5288beb557325a9b049c59f9980
junior-slv <dirceu.junior@edu.unifil.br>
2023-10-16 18:16:34 -0300

    teste

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const fs = require('node:fs');
-const path = require('node:path');
+const fs = require('fs');
+const path = require('path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
-
+const dotenv = require('dotenv');
+dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -11,40 +11,40 @@ const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+  }
 }
 
 client.once(Events.ClientReady, () => {
-	console.log('Ready!');
+  console.log('Ready!');
 });
 
 client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+  if (!interaction.isChatInputCommand()) return;
 
-	const command = client.commands.get(interaction.commandName);
+  const command = client.commands.get(interaction.commandName);
 
-	if (!command) return;
+  if (!command) return;
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'Você não tem permissão pra executar esse comando!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'Você não tem permissão pra executar esse comando!', ephemeral: true });
-		}
-	}
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({ content: 'Você não tem permissão pra executar esse comando!', ephemeral: true });
+    } else {
+      await interaction.reply({ content: 'Você não tem permissão pra executar esse comando!', ephemeral: true });
+    }
+  }
 });
 
-client.login(token);
+client.login(process.env.TOKEN); // Use a variável de ambiente para o token
